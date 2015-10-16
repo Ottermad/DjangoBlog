fd1b5b78c624b6f452f104b3bd7fb8722697db56
Charles Thomas <charlie.thomas@attwoodthomas.net>
2015-10-16 20:15:21 +0000

    add post and comment models

--- djangoblog/blog/models.py
+++ djangoblog/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(unique=True, max_length=30)
+    body = models.TextField()
+    author = models.ForeignKey(User)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(User)
+    body = models.CharField(max_length=300)
