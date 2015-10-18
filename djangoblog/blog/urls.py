09a8a5cf49b7570a0ed1e8440a875967da64d839
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2015-10-18 09:35:12 +0100

    add post_detail to urls.py

--- djangoblog/blog/urls.py
+++ djangoblog/blog/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^posts/([0-9]+)', views.post_detail, name='post_detail'),
     url(r'^posts/', views.post_listing, name='post_listing'),
     url(r'^$', views.index, name='index')
 ]
