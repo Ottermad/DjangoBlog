c626b51cced11c5021a6159db5473a70b98169b1
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2015-10-18 09:16:35 +0100

    rename posts_listing to post_listing

--- djangoblog/blog/views.py
+++ djangoblog/blog/views.py
@@ -5,7 +5,7 @@ def index(request):
     return render(request, 'blog/index.html')
 
 
-def posts_listing(request):
+def post_listing(request):
     # get all the posts
     posts = Post.objects.all().filter(is_draft=False).order_by("-publication_date")[:10]
     return render(request, 'blog/post_listing.html', {'posts': posts})
